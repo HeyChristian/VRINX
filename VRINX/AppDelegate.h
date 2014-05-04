2141d053afeacec613ffe9a0440cdd0b886d99bd
Christian Vazquez <colapsov@gmail.com>
2014-05-04 17:33:05 -0400

    Initial Commit

---
+++ VRINX/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  VRINX
+//
+//  Created by Christian Vazquez on 5/4/14.
+//  Copyright (c) 2014 Christian Vazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
